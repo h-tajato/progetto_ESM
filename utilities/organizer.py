2eccefd0e8534ed4c5f9de2cc7779cf4494d285e
Agda78 <agostinodamora85@gmail.com>
2025-06-09 11:24:29 +0200

    Aggiustamento del main

--- utilities/organizer.py
+++ utilities/organizer.py
@@ -1,10 +1,7 @@
 import os 
 import libs.spettrogram as sp
 
-
 path = '.\\guida'
-
-
 
 if __name__ == '__main__':
     os.system('rm -r ./input_spectrograms/*')
@@ -15,11 +12,12 @@ if __name__ == '__main__':
 
             new_path = f'.\\input_spectrograms\\{dir_name}\\{dir_name}.png'
             new_dir = f'./input_spectrograms/{dir_name}'
+            
             os.makedirs(f".\\input_spectrograms\\{dir_name}", exist_ok=True)
+
             spectrogram, shape_spec = sp.spectrify(complete_path)
+            
             if(sp.save_spectrogram(new_path, spectrogram, clip=False)):
                 print(f'[MAIN]\t{file} salvato con successo con dimensione ({shape_spec[0]}, {shape_spec[1]})')
             os.system(f'cp {complete_path} {new_dir}')
 
-
-        
